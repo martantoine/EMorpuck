07ad2a3b0f316e9feac957cf845c5b7074d45f53
Martantoine <martantoine@tuta.io>
2022-04-11 21:30:31 +0200

    karl khalil süss

--- src/main.c
+++ src/main.c
@@ -1,4 +1,34 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <motors.h>
+#include <camera/po8030.h>
+#include <chprintf.h>
+
+
+int main(void)
 {
-    return 0;
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    while (1) {
+        chThdSleepMilliseconds(1000);
+    }
+    // karl u suss
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
 }
